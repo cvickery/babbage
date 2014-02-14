570cb78bc6053d5d1916945d64fb8d341a6ac75b
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2014-02-14 00:10:11 -0500

    valentine

---
+++ 2014-02-14.php
@@ -0,0 +1,25 @@
+<html>
+  <head>
+    <title>Be My Valentine!</title>
+    <style type='text/css'>
+      html, body {
+        background-color: #f15;
+        color: lightyellow;
+        font-size:24px;
+        font-weight:bold;
+        font-family:Copperplate, Helvetica, fantasy;
+        margin: 0; padding: 0;
+      }
+      body {
+        width: 640;
+        margin: 1em;
+        padding: 1em;
+        border: 1px solid lightyellow;
+        border-radius: 12px;
+      }
+    </style>
+  </head>
+  <body>
+  Now is the time
+  </body>
+</html>
