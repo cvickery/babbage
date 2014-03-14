02f6064ea16d1e825dcfe9d9dc16b96726228d1f
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2014-03-14 14:02:01 -0400

    typo fix and better variable names

--- sms/index.php
+++ sms/index.php
@@ -3,13 +3,13 @@ require_once("Services/Twilio.php");
 
 if (isset($_POST['From']) && issset($_POST['Body']))
 {
-  $from   = $_POST['From'];
-  $msg    = $_POST['Body'];
-  $alert  = "SMS from $from");
+  $from       = $_POST['From'];
+  $body_msg   = $_POST['Body'];
+  $alert_msg  = "SMS from $from";
   $client = new Services_Twilio('ACc5146b2225441a0866c093e1eed457f0',
                                 'aba808e8fbfa0d09e3c77ae3eda0024b');
-  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $alert);
-  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $msg);
+  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $alert_msg);
+  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $body_msg);
 }
 
 ?>
