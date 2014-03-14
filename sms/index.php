632e41cf62f4b825490869c83bf2b025ab985440
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2014-03-14 13:56:34 -0400

    test first complete version

--- sms/index.php
+++ sms/index.php
@@ -1,14 +1,15 @@
 <?php
 require_once("Services/Twilio.php");
 
-error_log('Test ccv');
-foreach ($_POST as $key => $value)
-  error_log("$key => $value");
-
-$client = new Services_Twilio('ACc5146b2225441a0866c093e1eed457f0',
-                              'aba808e8fbfa0d09e3c77ae3eda0024b');
-$sms = $client->account->sms_messages->create(
-        '+13472897949', '+17182883809',
-        'You goddit now');
+if (isset($_POST['From']) && issset($_POST['Body']))
+{
+  $from   = $_POST['From'];
+  $msg    = $_POST['Body'];
+  $alert  = "SMS from $from");
+  $client = new Services_Twilio('ACc5146b2225441a0866c093e1eed457f0',
+                                'aba808e8fbfa0d09e3c77ae3eda0024b');
+  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $alert);
+  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $msg);
+}
 
 ?>
