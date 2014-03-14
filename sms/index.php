76aac424906c18061b417b6238f628554b30db68
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2014-03-14 13:01:39 -0400

    Initial test

---
+++ sms/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once("Services/Twilio.php");
+
+$client = new Services_Twilio('ACc5146b2225441a0866c093e1eed457f0',
+                              'aba808e8fbfa0d09e3c77ae3eda0024b');
+$sms = $client->account->sms_messages->create(
+        '+13472897949', '+17182883809',
+        'You goddit');
+?>
