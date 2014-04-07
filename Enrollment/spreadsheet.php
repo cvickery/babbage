a010b0aee28a0002a01f8f7bd483667f337365af
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2014-04-06 22:44:37 -0400

    Change from xlsx to zip for enrollments file

--- Enrollment/spreadsheet.php
+++ Enrollment/spreadsheet.php
@@ -1,20 +1,20 @@
 <?php
 
-$spreadsheet = 'enrollment.xlsx';
-if (file_exists($spreadsheet))
+$zip_file = 'enrollments.zip';
+if (file_exists($zip_file))
 {
   header("Cache-Control: public");
   header("Content-Description: File Transfer");
-  header("Content-Length: " . filesize($spreadsheet) . ";");
-  header("Content-Disposition: attachment; filename=$spreadsheet");
-  header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet; ");
+  header("Content-Length: " . filesize($zip_file) . ";");
+  header("Content-Disposition: attachment; filename=$zip_file");
+  header("Content-Type: application/zip");
   header("Content-Transfer-Encoding: UTF-8");
-  readfile($spreadsheet);
+  readfile($zip_file);
   exit;
 }
 else
 {
-  die('Spreadsheet not available');
+  die('Zip file not available');
 }
 
 ?>
