4f909fc99bd4616f172e369361d60692568c78aa
Christopher Vickery <Christopher.Vickery@qc.cuny.edu>
2015-02-04 22:34:44 -0500

    Update students and reduce forwarding to a single msg

--- sms/index.php
+++ sms/index.php
@@ -18,16 +18,24 @@ if (isset($_POST['From']) && isset($_POST['Body']))
   {
     $from .= ' (' . $valid_senders[$from] . ')';
   }
-  $body_msg   = $_POST['Body'];
   $alert_msg  = "SMS from $from";
+  $body_msg   = $_POST['Body'];
+  $msg = $alert_msg . $body_msg;
+  if (strlen($msg > 140))
+  {
+    $msg = substr($msg, 0, 136) . ' ...';
+  }
   $client = new Services_Twilio('ACc5146b2225441a0866c093e1eed457f0',
                                 'aba808e8fbfa0d09e3c77ae3eda0024b');
-  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $alert_msg);
-  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $body_msg);
+  $sms = $client->account->sms_messages->create('+13472897949', '+17182883809', $msg);
   if (! mail('cvickery@gmail.com', 'SMS Received', "$alert_msg\n$body_msg",
       "From: SMS on Babbage <Christopher.Vickery@qc.cuny.edu>"))
   {
     error_log("Failed to send email about $alert_msg");
   }
+}
+else
+{
+  echo "<h1>This is not a web page</h1>";
 }
 ?>
